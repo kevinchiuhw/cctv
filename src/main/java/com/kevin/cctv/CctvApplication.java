75314cdad10725d83ae6e92d02932411bd9b880e
kevinchiuhw <kevin.chiu@hwacom.com>
2025-04-01 15:59:44 +0800

    Initialize

---
+++ src/main/java/com/kevin/cctv/CctvApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.cctv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CctvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/cctv/CctvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.cctv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
