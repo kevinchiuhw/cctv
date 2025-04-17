da3a4adc8dcbff93c16d1b05d638d4b65e37cc81
kevinchiuhw <kevin.chiu@hwacom.com>
2025-04-17 14:29:28 +0800

    Add Fixed

--- src/main/java/com/kevin/cctv/boot/CctvApplication.java
+++ src/main/java/com/kevin/cctv/boot/CctvApplication.java
@@ -2,7 +2,9 @@ package com.kevin.cctv.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +12,11 @@ public class CctvApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CctvApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
